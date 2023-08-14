124ea83d505673d808c78220f5de43eb5f541247
NehuenCuenca <nehuen.cuenca@hotmail.com>
2023-08-14 10:53:50 -0300

    first commit, started the html structure, and applying base css styles

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <nav>
+      <button>random 🔄</button>
+    </nav>
+  </header>
+  
+  <main>
+    <div class="random-quote">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        sunt, maxime ex architecto voluptates laborum assumenda ipsam tenetur
+        perferendis aliquam. Tenetur commodi quas dicta recusandae nulla,
+        adipisci rem temporibus nihil.
+      </p>
+      <div class="author-and-tags">
+        <h3>Bill Gates</h3>
+        <h5>bussines</h5>
+      </div>
+    </div>
+  </main>
+
+  <footer>created by username - devChallenges.io</footer>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
